ff310221821ccd9541875acf351b5a9383c631a4
Alberto Seoane <albertoseoanesebe1993@gmail.com>
2026-03-23 20:53:50 +0100

    Prueba de login

--- cypress/e2e/pruebasparejas.cy.js
+++ cypress/e2e/pruebasparejas.cy.js
@@ -2,8 +2,9 @@ describe('template spec', () => {
   it('passes', () => {
     cy.visit('https://the-internet.herokuapp.com/')
     cy.get(':nth-child(21) > a').click()
-    cy.get('[name="username"]').type("AlbertoSeoane1993")
-    cy.get('[name="password"]').type("Alberto")
+    cy.get('[name="username"]').type("tomsmith")
+    cy.get('[name="password"]').type("SuperSecretPassword!")
     cy.get('.fa') .click()
+    cy.get('#flash').contains("You logged into a secure area!")
   })
 })
